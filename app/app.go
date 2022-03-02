fcd7889cc3a61e2c74b11c8878d73d3c02c6fe76
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-03-02 12:02:00 +0700

    update to use auth

--- app/app.go
+++ app/app.go
@@ -8,15 +8,26 @@ import (
 	"time"
 
 	"github.com/fajarabdillahfn/banking_app/domain"
+	"github.com/fajarabdillahfn/banking_app/logger"
 	"github.com/fajarabdillahfn/banking_app/service"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
 func sanityCheck() {
-	if os.Getenv("SERVER_ADDRESS") == "" ||
-		os.Getenv("SERVER_PORT") == "" {
-		log.Fatal("Environment variable not defined....")
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+	for _, k := range envProps {
+		if os.Getenv(k) == "" {
+			log.Fatal(fmt.Sprintf("Environment variable %s not defined. Terminating application...", k))
+		}
 	}
 }
 
@@ -36,14 +47,30 @@ func Start() {
 	ah := AccountHandler{service: service.NewAccountService(accountRepositoryDb)}
 
 	// define routes
-	router.HandleFunc("/customers", ch.GetAllCustomers).Methods(http.MethodGet)
-	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.GetCustomer).Methods(http.MethodGet)
-	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
-	router.HandleFunc("/customers/{customer_id:[0-9]+}/account/{account_id:[0-9]+}", ah.MakeTransaction).Methods(http.MethodPost)
+	router.
+		HandleFunc("/customers", ch.GetAllCustomers).
+		Methods(http.MethodGet).
+		Name("GetAllCustomers")
+	router.
+		HandleFunc("/customers/{customer_id:[0-9]+}", ch.GetCustomer).
+		Methods(http.MethodGet).
+		Name("GetCustomer")
+	router.
+		HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).
+		Methods(http.MethodPost).
+		Name("NewAccount")
+	router.
+		HandleFunc("/customers/{customer_id:[0-9]+}/account/{account_id:[0-9]+}", ah.MakeTransaction).
+		Methods(http.MethodPost).
+		Name("Newtransaction")
+
+	am := AuthMiddleware{domain.NewAuthRepository()}
+	router.Use(am.AuthorizationHandler())
 
 	// starting server
 	address := os.Getenv("SERVER_ADDRESS")
 	port := os.Getenv("SERVER_PORT")
+	logger.Info(fmt.Sprintf("Starting server on %s:%s ...", address, port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
 
